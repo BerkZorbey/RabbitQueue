fabb196871a4055b17ad619c9ae2d57fd85b158e
Taha Berk Yeşilalioğlu <berktaha1994@gmail.com>
2022-11-04 13:57:43 +0300

    Proje dosyası ekle.

---
+++ Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+var queue = "BerkQueue";
+channel.QueueDeclare(queue: queue, durable: false, exclusive: false, autoDelete: false,null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = Encoding.UTF8.GetString(ea.Body.ToArray());
+    Console.WriteLine(body);
+};
+
+channel.BasicConsume(queue, autoAck: true, consumer: consumer);
+Console.Read();
---
+++ Procedure/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var person = new
+{
+    Name = "Ad",
+    SurName = "Soyad",
+    Tc = "11111111111",
+    Email = "a@a.com"
+};
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+var queue = "BerkQueue";
+channel.QueueDeclare(queue: queue, durable: false, exclusive: false, autoDelete: false, null);
+
+string message = JsonSerializer.Serialize(person);
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange:"",routingKey:queue,body:body);
